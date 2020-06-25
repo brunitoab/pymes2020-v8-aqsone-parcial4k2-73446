2fab76dd098f6efaf76ec94d5b39b69a407ead4f
ItsMeBrunito <brunito_aresu@hotmail.com>
2020-06-25 15:42:46 -0300

    1 file modified, 4 added

---
+++ src/app/services/presupuesto.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+  HttpParams
+} from "@angular/common/http";
+import { of } from "rxjs";
+import { Presupuesto } from "../models/presupuesto";
+
+@Injectable({
+  providedIn: "root"
+})
+export class PresupuestoService {
+  resourceUrl: string;
+  constructor(private httpClient: HttpClient) {
+    // la barra al final del resourse url es importante para los metodos que concatenan el id del recurso (GetById, Put)
+    this.resourceUrl = "https://pavii.ddns.net/api/presupuestosttps://pavii.ddns.net/api/articulos/";
+  }
+}
